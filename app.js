2c6ad4f9db59d3960a28b56df114624b3f03a0b4
rodry <rodrigomceraolo@gmail.com>
2023-10-17 17:34:33 -0300

    mercadoLiebre

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3030;
-app.use(express.estatic(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
 app.get("/ofertas", (req, res) => res.sendFile(path.join(__dirname, "views", "ofertas.html")));
 app.get("/tiendas", (req, res) => res.sendFile(path.join(__dirname, "views", "tiendas.html")));
